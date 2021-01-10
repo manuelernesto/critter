01d498f82bc0fdfb76fc70ba468615c877ee4e08
manuelernesto <manuelernesto@outlook.com>
2021-01-11 00:13:33 +0100

    set employee availability

--- src/main/java/com/udacity/jdnd/course3/critter/controller/UserController.java
+++ src/main/java/com/udacity/jdnd/course3/critter/controller/UserController.java
@@ -57,7 +57,7 @@ public class UserController {
 
     @PutMapping("/employee/{employeeId}")
     public void setAvailability(@RequestBody Set<DayOfWeek> daysAvailable, @PathVariable long employeeId) {
-        throw new UnsupportedOperationException();
+        employeeService.setEmployeeAvailability(daysAvailable, employeeId);
     }
 
     @GetMapping("/employee/availability")
--- src/main/java/com/udacity/jdnd/course3/critter/service/EmployeeService.java
+++ src/main/java/com/udacity/jdnd/course3/critter/service/EmployeeService.java
@@ -4,17 +4,26 @@ import com.udacity.jdnd.course3.critter.entity.Employee;
 import com.udacity.jdnd.course3.critter.repository.EmployeeRepository;
 import org.springframework.stereotype.Service;
 
+import java.time.DayOfWeek;
+import java.util.Set;
+
 @Service
 public class EmployeeService {
+    private final EmployeeRepository employeeRepository;
 
     public EmployeeService(EmployeeRepository employeeRepository) {
         this.employeeRepository = employeeRepository;
     }
 
-    private final EmployeeRepository employeeRepository;
 
     public Employee saveEmployee(Employee employee) {
         return employeeRepository.save(employee);
+    }
+
+    public void setEmployeeAvailability(Set<DayOfWeek> daysAvailable, long employeeId) {
+        Employee employee = employeeRepository.getOne(employeeId);
+        employee.setDaysAvailable(daysAvailable);
+        employeeRepository.save(employee);
     }
 
 }
